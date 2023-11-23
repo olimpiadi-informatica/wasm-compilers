f5176864863e4d646e32f079384b61c18899934a
Luca Versari <veluca93@gmail.com>
2023-11-23 23:15:18 +0100

    Working version + packing of needed files

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -xe
 
@@ -6,8 +6,9 @@ WASI_SDK_PREFIX=${WASI_SDK_PATH:-/opt/wasi-sdk/}
 
 DIR=$PWD
 
+
 pushd llvm
-git checkout . 
+git checkout .
 patch -p1 < ../llvm.patch
 popd
 
@@ -18,7 +19,7 @@ $WASI_SDK_PREFIX/bin/clang++ -fno-exceptions -std=c++20 \
 rm -rf llvm-build
 mkdir -p llvm-build
 pushd llvm-build
-cmake ../llvm/llvm -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+cmake ../llvm/llvm -G Ninja -DCMAKE_BUILD_TYPE=Release \
   -DWASI_SDK_PREFIX=${WASI_SDK_PREFIX} \
   -DCMAKE_TOOLCHAIN_FILE=${WASI_SDK_PREFIX}/share/cmake/wasi-sdk.cmake \
   -DLLVM_TARGETS_TO_BUILD=WebAssembly -DLLVM_ENABLE_PROJECTS="clang;lld" \
@@ -27,6 +28,17 @@ cmake ../llvm/llvm -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo \
   -DCMAKE_CXX_FLAGS="-I$DIR -fno-exceptions" -DLLVM_INCLUDE_TESTS=OFF \
   -DLLVM_BUILD_LLVM_DYLIB=OFF -DLLVM_INCLUDE_EXAMPLES=OFF -DLLVM_ENABLE_PIC=OFF \
   -DLLVM_INCLUDE_UTILS=OFF -DLLVM_BUILD_UTILS=OFF -DLLVM_ENABLE_PLUGINS=OFF \
-  -DCLANG_PLUGIN_SUPPORT=OFF -DCMAKE_EXE_LINKER_FLAGS="-lwasishim -L$DIR/build"
+  -DCLANG_PLUGIN_SUPPORT=OFF \
+  -DCMAKE_EXE_LINKER_FLAGS="-Wl,--stack-first -Wl,-z -Wl,stack-size=1048576 -lwasishim -L$DIR/build"
 ninja
 popd
+
+rm -rf build/output
+mkdir -p build/output/root
+cp -Lr ${WASI_SDK_PREFIX}/share/wasi-sysroot/* build/output/root/
+rm -rf build/output/root/share/wasm32-wasi-threads build/output/root/lib/wasm32-wasi-threads
+cp -Lr llvm-build/lib/clang/18/include build/output/root/clang-include
+cp -Lr llvm-build/bin/clang-18 build/output/
+cp -Lr llvm-build/bin/lld build/output/wasm-ld
+
+tar c build/output | brotli > build/output.tar.br
