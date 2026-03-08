9c4affd33cb7015e73c3683db08166a9cc05c25c
Luca Versari <veluca@google.com>
2026-03-08 18:57:43 +0100

    Fix test.sh for newer wasmtime

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ ROOT=$PWD/build/output
 CPPROOT=$ROOT/cpp
 PYROOT=$ROOT/python
 
-$(which wasmtime) -W threads=y -S threads=y --dir $PYROOT::/ \
+$(which wasmtime) -W threads=y -S threads=y -W shared-memory=y --dir $PYROOT::/ \
   --env PYTHONPATH=/lib/python-3.13 \
   $PYROOT/bin/python3.13.wasm \
   -c "import json; print(json.dumps('hello'))"
@@ -17,7 +17,7 @@ rm -rf $DIR
 mkdir -p $DIR
 cp -r $CPPROOT/* $DIR
 
-cat > $DIR/main.cc << EOF
+cat >$DIR/main.cc <<EOF
 #include <bits/stdc++.h>
 int main(int argc, char **argv) {
   std::vector<std::string> v;
@@ -34,7 +34,7 @@ int main(int argc, char **argv) {
 }
 EOF
 
-$(which wasmtime) -W threads=y -S threads=y --dir $DIR::/ \
+$(which wasmtime) -W threads=y -S threads=y -W shared-memory=y --dir $DIR::/ \
   $DIR/bin/llvm clang++ -cc1 -isysroot / \
   "-resource-dir" "lib/clang/20" -I "/include/c++/15.0.0/wasm32-wasip1/" -I "/include/c++/15.0.0/" "-isysroot" "/" \
   "-internal-isystem" "lib/clang/20/include" "-internal-isystem" "/include/wasm32-wasip1-threads" "-internal-isystem" "/include" \
@@ -42,7 +42,7 @@ $(which wasmtime) -W threads=y -S threads=y --dir $DIR::/ \
   "-stdlib=libstdc++" \
   -O2 -emit-obj main.cc -o main.wasm
 
-$(which wasmtime) -W threads=y -S threads=y --dir $DIR::/ \
+$(which wasmtime) -W threads=y -S threads=y -W shared-memory=y --dir $DIR::/ \
   $DIR/bin/llvm wasm-ld \
   -L /lib/wasm32-wasip1-threads/ /lib/clang/20/lib/wasm32-unknown-wasip1-threads/libclang_rt.builtins.a \
   -lc /lib/wasm32-wasip1-threads/crt1.o \
@@ -50,4 +50,4 @@ $(which wasmtime) -W threads=y -S threads=y --dir $DIR::/ \
   -z stack-size=1048576 --shared-memory --import-memory --export-memory --max-memory=4294967296 \
   main.wasm -o main
 
-$(which wasmtime) -W threads=y -S threads=y $DIR/main a b c d <<< 13845
+$(which wasmtime) -W threads=y -S threads=y -W shared-memory=y $DIR/main a b c d <<<13845
